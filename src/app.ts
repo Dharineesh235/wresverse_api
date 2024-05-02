2a7f399696671d8e2945d04e5dd426efc40a7481
Dharineesh G G <dharineeshgg@Dharineeshs-MacBook-Air.local>
2024-05-02 16:55:32 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,4 +9,4 @@ export const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, ()=>console.log(`http://localhost:${PORT}`));
+app.listen(PORT, ()=>console.log(`Server Running...`));
